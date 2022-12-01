5caecfa7ce28002d63f70863bee331d6119a1253
yutteee <yusaku_nakamura_aa@icloud.com>
2022-12-01 15:20:36 +0900

    Initial Commit

---
+++ swift-tutrial/swift_tutrialApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_tutrialApp.swift
+//  swift-tutrial
+//
+//  Created by nakamura yusaku on 2022/12/01.
+//
+
+import SwiftUI
+
+@main
+struct swift_tutrialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
